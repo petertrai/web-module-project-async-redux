e4c4933187115e718e2c89c2a61eec0eb49297e5
petertrai <petertrai@gmail.com>
2023-06-13 17:40:15 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
-
+import data from './data/gifsdata'
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Welcome to the GIFS loader</h1>
+      <form>
+    <input type='text'/><br/>
+    <button>Search</button>
+    </form>
+    <div className="gif-card">
+    {data.map(gif => {
+      return( <div key={gif.id}>{gif.title}, {gif.url}</div>)
+    })}
+    
+    </div>
     </div>
   );
 }
 
 export default App;
+
+// api_key=r0IZOEjRwBLcp6GkmpXGk8r0CAbmM16h
+// https://api.giphy.com/v1/gifs/search	
+// https://api.giphy.com/v1/gifs/search?api_key=r0IZOEjRwBLcp6GkmpXGk8r0CAbmM16h&q=dogs
---
+++ src/data/gifsdata.js
@@ -0,0 +1,56 @@
+const data = [
+    {
+        type: "gif",
+        id: "vogvJKBOwkMmYqwjfq",
+        url: "https://giphy.com/gifs/viralhog-viral-hog-vogvJKBOwkMmYqwjfq",
+        slug: "viralhog-viral-hog-vogvJKBOwkMmYqwjfq",
+        bitly_gif_url: "https://gph.is/g/E3LLp8N",
+        bitly_url: "https://gph.is/g/E3LLp8N",
+        embed_url: "https://giphy.com/embed/vogvJKBOwkMmYqwjfq",
+        username: "viralhog",
+        source: "https://viralhog.com",
+        title: "Dog GIF by ViralHog",
+        rating: "pg",
+        content_url: "",
+        source_tld: "viralhog.com",
+        source_post_url: "https://viralhog.com",
+        is_sticker: 0,
+        import_datetime: "2021-01-21 19:21:58",
+        trending_datetime: "0000-00-00 00:00:00",
+        images: {
+            original: {
+                url: "https://media0.giphy.com/media/vogvJKBOwkMmYqwjfq/giphy.gif?cid=a448e1e7a1uhtnpgzp6ltibnvm2rtk23bgowbvecbtfc74wm&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+                hash: "a0ccfb6620c309e3a1d82232157ce394"
+            }
+        }
+    },
+    {
+        type: "gif",
+        id: "vogvJKBwkMmYqwjfq",
+        url: "https://giphy.com/gifs/viralhog-viral-hog-vogvJKBOwkMmYqwjfq",
+        slug: "viralhog-viral-hog-vogvJKBOwkMmYqwjfq",
+        bitly_gif_url: "https://gph.is/g/E3LLp8N",
+        bitly_url: "https://gph.is/g/E3LLp8N",
+        embed_url: "https://giphy.com/embed/vogvJKBOwkMmYqwjfq",
+        username: "viralhog",
+        source: "https://viralhog.com",
+        title: "Dog GIF by ViralHog",
+        rating: "pg",
+        content_url: "",
+        source_tld: "viralhog.com",
+        source_post_url: "https://viralhog.com",
+        is_sticker: 0,
+        import_datetime: "2021-01-21 19:21:58",
+        trending_datetime: "0000-00-00 00:00:00",
+        images: {
+            original: {
+                url: "https://media0.giphy.com/media/vogvJKBOwkMmYqwjfq/giphy.gif?cid=a448e1e7a1uhtnpgzp6ltibnvm2rtk23bgowbvecbtfc74wm&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+                hash: "a0ccfb6620c309e3a1d82232157ce394"
+            }
+        }
+    }
+
+
+]
+
+export default data
